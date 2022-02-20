fd77f39a389a618d4ba6032c1ff77c1c050ae1d9
Valters <valters.tomsons@protonmail.com>
2022-02-20 04:16:55 +0200

    Resolve temp directory for all platforms

--- src/OrbisDbTools.Utils/ClientConfig.cs
+++ src/OrbisDbTools.Utils/ClientConfig.cs
@@ -1,7 +1,28 @@
-namespace OrbisDbTools.Utils
+using System.Runtime.InteropServices;
+namespace OrbisDbTools.Utils;
+
+public static class ClientConfig
 {
-    public static class ClientConfig
+    public static Uri TempDirectory { get; }
+
+    private const string ToolsFolderName = "orbisdbtools";
+
+    static ClientConfig()
     {
-        public static readonly Uri TempDirectory = new("/tmp/orbisdbtools");
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var tmpdir = Environment.GetEnvironmentVariable("%Temp%");
+            TempDirectory = new($"{tmpdir}/{ToolsFolderName}");
+        }
+        else
+        {
+            var tmpdir = Environment.GetEnvironmentVariable("TMPDIR");
+            if (!string.IsNullOrWhiteSpace(tmpdir))
+            {
+                TempDirectory = new($"{tmpdir}/{ToolsFolderName}");
+            }
+
+            TempDirectory = new($"/tmp/{ToolsFolderName}");
+        }
     }
 }
