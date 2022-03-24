e0ea1980100e9c22bc1ab280792e5652aab479ca
Valters <valters.tomsons@protonmail.com>
2022-03-25 01:38:12 +0200

    Add /user/app constant

--- src/OrbisDbTools.PS4/Constants/OrbisSystemPaths.cs
+++ src/OrbisDbTools.PS4/Constants/OrbisSystemPaths.cs
@@ -8,6 +8,7 @@ public struct OrbisSystemPaths
     public const string AccountsFolderPath = "/system_data/priv/home/";
     public const string AppMetaFolderPath = "/system_data/priv/appmeta/";
 
+    public const string UserAppPath = "/user/app/";
     public const string UserAppMetadataPath = "/user/appmeta/";
     public const string UserExternalAppMetadata = $"{UserAppMetadataPath}external/";
 
