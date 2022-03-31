f481f5c785d2a3c2b2c78209ef5f8b5dacb5bf03
Valters <valters.tomsons@protonmail.com>
2022-04-01 00:57:24 +0300

    Prevent empty title names

--- src/OrbisDbTools.PS4/Models/AppTitle.cs
+++ src/OrbisDbTools.PS4/Models/AppTitle.cs
@@ -4,11 +4,19 @@ namespace OrbisDbTools.PS4.Models;
 
 public record AppTitle : IEquatable<AppTitle>
 {
+    private string titleName = string.Empty;
+
     public AppTitle() { }
     public AppTitle(string titleId, string titleName) { TitleId = titleId; TitleName = titleName; }
 
     public string TitleId { get; init; } = string.Empty;
-    public string TitleName { get; init; } = string.Empty;
+
+    public string TitleName
+    {
+        get { return titleName; }
+        set { if (value.Length > 1) titleName = value; }
+    }
+
     public string ContentId { get; init; } = string.Empty;
     public string MetaDataPath { get; init; } = string.Empty;
     public string? Category { get; init; }
