351d57e3f8e1bb2f756af2371de815edc8f9c651
Valters <valters.tomsons@protonmail.com>
2022-07-31 15:10:33 +0300

    Fix nullable warnings

--- src/OrbisDbTools.PS4/Models/AddContTblRow.cs
+++ src/OrbisDbTools.PS4/Models/AddContTblRow.cs
@@ -5,16 +5,16 @@ namespace OrbisDbTools.PS4.Models;
 [Table("addcont")]
 public class AddContTblRow
 {
-	[Key]
-	public int id { get; set; }
+    [Key]
+    public int id { get; set; }
 
-	public string title_id { get; set; }
-	public string dir_name { get; set; }
+    public string title_id { get; set; } = string.Empty;
+    public string dir_name { get; set; } = string.Empty;
 
-	public string content_id { get; set; }
+    public string content_id { get; set; } = string.Empty;
 
-	public string title { get; set; }
-	public int version { get; set; }
-	public string attribute { get; set; } = "01.00";
-	public int status { get; set; }
+    public string title { get; set; } = string.Empty;
+    public int version { get; set; }
+    public string attribute { get; set; } = "01.00";
+    public int status { get; set; }
 }
